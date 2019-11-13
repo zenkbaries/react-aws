fcc447f8d6122aaa3ec457d2e40a6a0e18846b6d
John Towery <kbaries@gmail.com>
2019-11-12 20:15:05 -0700

    edit app.js to show amplify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import react_logo from './logo_react.svg';
+import amplify_logo from './logo_amplify.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="row">
+            <img src={react_logo} className="App-logo-react" alt="react logo" />
+            <img src={amplify_logo} className="App-logo-amplify" alt="amplify logo" />
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Running React on AWS's Amplify made easy.
         </p>
         <a
           className="App-link"
@@ -17,6 +21,14 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://aws.amazon.com/amplify/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn AWS Amplify
         </a>
       </header>
     </div>
